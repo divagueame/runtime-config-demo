2b71ed7ec7e2becc482032bc43938158b9ab43d4
Martin Arce <m.arce@joviva.de>
2024-12-19 22:42:25 +0100

    Docs example

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  runtimeConfig: {
+    apiSecret: "", // can be overridden by NUXT_API_SECRET environment variable
+    public: {
+      apiBase: "meow", // can be overridden by NUXT_PUBLIC_API_BASE environment variable
+    },
+  },
+});
