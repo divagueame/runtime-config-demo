3f9780cd0d93a75d16e5f9054a6e07cebfd716b9
Martin Arce <m.arce@joviva.de>
2024-12-19 22:44:04 +0100

    Add nuxt-security module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+
   runtimeConfig: {
     apiSecret: "", // can be overridden by NUXT_API_SECRET environment variable
     public: {
       apiBase: "", // can be overridden by NUXT_PUBLIC_API_BASE environment variable
     },
   },
+
+  modules: ["nuxt-security"],
 });
